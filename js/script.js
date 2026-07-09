8d1a02449c6837e1ee873fafb41a543222f30728
njegovanaleksandra4 <njegovan.aleksandra4@gmail.com>
2026-07-09 14:38:06 +0200

    Responzivan meni za telefon

--- js/script.js
+++ js/script.js
@@ -1,25 +1,37 @@
-const menuToggle = document.getElementById('menuToggle');
-    const nav = document.querySelector('nav');
+const menuBtn=document.getElementById("menuBtn");
+const nav=document.querySelector("nav");
+const overlay = document.getElementById("overlay");
+const links = document.querySelectorAll("nav a");
+/*aktiviranje menija*/
+menuBtn.addEventListener("click",()=>{
 
-    menuToggle.addEventListener('click', () => {
-        nav.classList.toggle('active');
-        
-        // Menjamo izgled ikonice iz hamburgera u X i obrnuto
-        if (nav.classList.contains('active')) {
-            menuToggle.innerHTML = '✕';
-            document.body.style.overflow = 'hidden'; // zaključaj skrolovanje pozadine
-        } else {
-            menuToggle.innerHTML = '☰';
-            document.body.style.overflow = ''; // vrati skrolovanje
-        }
+    nav.classList.toggle("active");
+    overlay.classList.toggle("active");
+    if(nav.classList.contains("active")){
+        menuBtn.innerHTML="✕";
+        document.body.style.overflow = 'hidden';
+    }else{
+        menuBtn.innerHTML="☰";
+    }
+
+});
+/*Gasenje menija kad se klikne sa strane */
+overlay.addEventListener("click",()=>{
+
+    nav.classList.remove("active");
+    overlay.classList.remove("active");
+    menuBtn.innerHTML="☰";
+
+});
+/*Gasenje menija kad se klikne na link */
+links.forEach(link=>{
+
+    link.addEventListener("click",()=>{
+
+        nav.classList.remove("active");
+        overlay.classList.remove("active");
+        menuBtn.innerHTML="☰";
+
     });
-    
-    // Opciono: Ako klikneš na link, zatvori meni
-    const navLinks = document.querySelectorAll('nav a');
-    navLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            nav.classList.remove('active');
-            menuToggle.innerHTML = '☰';
-            document.body.style.overflow = '';
-        });
-    });
+   
+});
